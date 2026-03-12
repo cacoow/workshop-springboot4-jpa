c0c0bff0902c1528c85e0d63efb0873c90f28dcb
Caco <feeenergy1234@gmail.com>
2026-03-12 12:21:46 -0300

    Payment, one-to-one association

--- src/main/java/com/fadelalloys/course/config/TestConfig.java
+++ src/main/java/com/fadelalloys/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.fadelalloys.course.entities.Category;
 import com.fadelalloys.course.entities.Order;
 import com.fadelalloys.course.entities.OrderItem;
+import com.fadelalloys.course.entities.Payment;
 import com.fadelalloys.course.entities.Product;
 import com.fadelalloys.course.entities.User;
 import com.fadelalloys.course.entities.enums.OrderStatus;
@@ -81,8 +82,9 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
-	
-	
-
 }
